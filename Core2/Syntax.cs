31ea67e13071eb75ec0f14516b0c8b42d7275263
curefate <66023115+curefate@users.noreply.github.com>
2025-11-14 15:39:51 +0100

    + visitor初版未测试

--- Core2/Syntax.cs
+++ Core2/Syntax.cs
@@ -6,6 +6,7 @@ namespace Narratoria.Core
         public int Line { get; set; }
         public int Column { get; set; }
         public abstract T Accept<T>(IASTVisitor<T> visitor);
+        public abstract List<ASTNode> Children { get; }
     }
 
     public class AST_Program : ASTNode
@@ -14,12 +15,14 @@ namespace Narratoria.Core
         public List<AST_Statement> TopLevelStatements { get; } = [];
         public List<AST_LabelBlock> Labels { get; } = [];
         public override T Accept<T>(IASTVisitor<T> visitor) => visitor.VisitProgram(this);
+        public override List<ASTNode> Children => [.. Imports, .. TopLevelStatements, .. Labels];
     }
 
     public class AST_Import : ASTNode
     {
         public required Token Path { get; init; }
         public override T Accept<T>(IASTVisitor<T> visitor) => visitor.VisitImport(this);
+        public override List<ASTNode> Children => [];
     }
 
     public class AST_LabelBlock : ASTNode
@@ -27,6 +30,7 @@ namespace Narratoria.Core
         public required Token LabelName { get; init; }
         public List<AST_Statement> Statements { get; } = [];
         public override T Accept<T>(IASTVisitor<T> visitor) => visitor.VisitLabelBlock(this);
+        public override List<ASTNode> Children => [.. Statements];
     }
 
     public abstract class AST_Statement : ASTNode { }
@@ -37,12 +41,14 @@ namespace Narratoria.Core
         public required AST_FString Text { get; init; }
         // public List<string> Tags { get; } = [];
         public override T Accept<T>(IASTVisitor<T> visitor) => visitor.VisitDialogue(this);
+        public override List<ASTNode> Children => [Text];
     }
 
     public class AST_Menu : AST_Statement
     {
         public List<AST_MenuItem> Items { get; } = [];
         public override T Accept<T>(IASTVisitor<T> visitor) => visitor.VisitMenu(this);
+        public override List<ASTNode> Children => [.. Items];
     }
 
     public class AST_MenuItem : ASTNode
@@ -50,18 +56,21 @@ namespace Narratoria.Core
         public required AST_FString Text { get; init; }
         public List<AST_Statement> Body { get; } = [];
         public override T Accept<T>(IASTVisitor<T> visitor) => visitor.VisitMenuItem(this);
+        public override List<ASTNode> Children => [Text, .. Body];
     }
 
     public class AST_Jump : AST_Statement
     {
         public required Token TargetLabel { get; init; }
         public override T Accept<T>(IASTVisitor<T> visitor) => visitor.VisitJump(this);
+        public override List<ASTNode> Children => [];
     }
 
     public class AST_Tour : AST_Statement
     {
         public required Token TargetLabel { get; init; }
         public override T Accept<T>(IASTVisitor<T> visitor) => visitor.VisitTour(this);
+        public override List<ASTNode> Children => [];
     }
 
     public class AST_Call : AST_Statement
@@ -69,6 +78,7 @@ namespace Narratoria.Core
         public required Token FunctionName { get; init; }
         public List<AST_Expression> Arguments { get; } = [];
         public override T Accept<T>(IASTVisitor<T> visitor) => visitor.VisitCall(this);
+        public override List<ASTNode> Children => [.. Arguments];
     }
 
     public class AST_Assign : AST_Statement
@@ -77,6 +87,7 @@ namespace Narratoria.Core
         public required Token Operator { get; init; }
         public required AST_Expression Value { get; init; }
         public override T Accept<T>(IASTVisitor<T> visitor) => visitor.VisitAssign(this);
+        public override List<ASTNode> Children => [Value];
     }
 
     public class AST_If : AST_Statement
@@ -85,6 +96,7 @@ namespace Narratoria.Core
         public List<AST_Statement> ThenBlock { get; } = [];
         public List<AST_Statement>? ElseBlock { get; set; }
         public override T Accept<T>(IASTVisitor<T> visitor) => visitor.VisitIf(this);
+        public override List<ASTNode> Children => ElseBlock != null ? [Condition, .. ThenBlock, .. ElseBlock!] : [Condition, .. ThenBlock];
     }
 
     public abstract class AST_Expression : ASTNode { }
@@ -94,6 +106,7 @@ namespace Narratoria.Core
         public required AST_Expr_And Left { get; init; }
         public List<(Token Operator, AST_Expr_And Right)> Rights { get; } = [];
         public override T Accept<T>(IASTVisitor<T> visitor) => visitor.VisitExprOr(this);
+        public override List<ASTNode> Children => [Left, .. Rights.Select(r => r.Right)];
     }
 
     public class AST_Expr_And : AST_Expression
@@ -101,6 +114,7 @@ namespace Narratoria.Core
         public required AST_Expr_Equality Left { get; init; }
         public List<(Token Operator, AST_Expr_Equality Right)> Rights { get; } = [];
         public override T Accept<T>(IASTVisitor<T> visitor) => visitor.VisitExprAnd(this);
+        public override List<ASTNode> Children => [Left, .. Rights.Select(r => r.Right)];
     }
 
     public class AST_Expr_Equality : AST_Expression
@@ -109,6 +123,7 @@ namespace Narratoria.Core
         public required Token? Operator { get; init; }
         public required AST_Expr_Comparison? Right { get; init; }
         public override T Accept<T>(IASTVisitor<T> visitor) => visitor.VisitExprEquality(this);
+        public override List<ASTNode> Children => Right != null ? [Left, Right] : [Left];
     }
 
     public class AST_Expr_Comparison : AST_Expression
@@ -117,6 +132,7 @@ namespace Narratoria.Core
         public required Token? Operator { get; init; }
         public required AST_Expr_Additive? Right { get; init; }
         public override T Accept<T>(IASTVisitor<T> visitor) => visitor.VisitExprComparison(this);
+        public override List<ASTNode> Children => Right != null ? [Left, Right] : [Left];
     }
 
     public class AST_Expr_Additive : AST_Expression
@@ -124,6 +140,7 @@ namespace Narratoria.Core
         public required AST_Expr_Multiplicative Left { get; init; }
         public List<(Token Operator, AST_Expr_Multiplicative Right)> Rights { get; } = [];
         public override T Accept<T>(IASTVisitor<T> visitor) => visitor.VisitExprAdditive(this);
+        public override List<ASTNode> Children => [Left, .. Rights.Select(r => r.Right)];
     }
 
     public class AST_Expr_Multiplicative : AST_Expression
@@ -131,6 +148,7 @@ namespace Narratoria.Core
         public required AST_Expr_Power Left { get; init; }
         public List<(Token Operator, AST_Expr_Power Right)> Rights { get; } = [];
         public override T Accept<T>(IASTVisitor<T> visitor) => visitor.VisitExprMultiplicative(this);
+        public override List<ASTNode> Children => [Left, .. Rights.Select(r => r.Right)];
     }
 
     public class AST_Expr_Power : AST_Expression
@@ -138,6 +156,7 @@ namespace Narratoria.Core
         public required AST_Expr_Unary Base { get; init; }
         public List<AST_Expr_Unary> Exponents { get; } = [];
         public override T Accept<T>(IASTVisitor<T> visitor) => visitor.VisitExprPower(this);
+        public override List<ASTNode> Children => [Base, .. Exponents];
     }
 
     public class AST_Expr_Unary : AST_Expression
@@ -145,6 +164,7 @@ namespace Narratoria.Core
         public required Token? Operator { get; init; }
         public required AST_Expr_Primary Primary { get; init; }
         public override T Accept<T>(IASTVisitor<T> visitor) => visitor.VisitExprUnary(this);
+        public override List<ASTNode> Children => [Primary];
     }
 
     public abstract class AST_Expr_Primary : AST_Expression { }
@@ -153,6 +173,7 @@ namespace Narratoria.Core
     {
         public required Token Value { get; init; }
         public override T Accept<T>(IASTVisitor<T> visitor) => visitor.VisitLiteral(this);
+        public override List<ASTNode> Children => [];
     }
 
     public class AST_FString : AST_Expr_Primary
@@ -160,18 +181,21 @@ namespace Narratoria.Core
         public List<Token> Fragments { get; } = [];
         public List<AST_Expression> Embeds { get; } = [];
         public override T Accept<T>(IASTVisitor<T> visitor) => visitor.VisitFString(this);
+        public override List<ASTNode> Children => [.. Embeds];
     }
 
     public class AST_EmbedCall : AST_Expr_Primary
     {
         public required AST_Call Call { get; init; }
         public override T Accept<T>(IASTVisitor<T> visitor) => visitor.VisitEmbedCall(this);
+        public override List<ASTNode> Children => [Call];
     }
 
     public class AST_EmbedExpr : AST_Expr_Primary
     {
         public required AST_Expression Expression { get; init; }
         public override T Accept<T>(IASTVisitor<T> visitor) => visitor.VisitEmbedExpr(this);
+        public override List<ASTNode> Children => [Expression];
     }
 
     // =========================== Visitor ===========================
@@ -207,31 +231,160 @@ namespace Narratoria.Core
 
     public abstract class SyntaxBaseVisitor<T> : IASTVisitor<T>
     {
-        public virtual T Visit(ASTNode node) => node.Accept(this);
-        public virtual T VisitProgram(AST_Program node) => default!;
-        public virtual T VisitImport(AST_Import node) => default!;
-        public virtual T VisitLabelBlock(AST_LabelBlock node) => default!;
-        public virtual T VisitDialogue(AST_Dialogue node) => default!;
-        public virtual T VisitMenu(AST_Menu node) => default!;
-        public virtual T VisitMenuItem(AST_MenuItem node) => default!;
-        public virtual T VisitJump(AST_Jump node) => default!;
-        public virtual T VisitTour(AST_Tour node) => default!;
-        public virtual T VisitCall(AST_Call node) => default!;
-        public virtual T VisitAssign(AST_Assign node) => default!;
-        public virtual T VisitIf(AST_If node) => default!;
+        public T Visit(ASTNode node)
+        {
+            return node switch
+            {
+                AST_Program n => VisitProgram(n),
+                AST_Import n => VisitImport(n),
+                AST_LabelBlock n => VisitLabelBlock(n),
+                AST_Dialogue n => VisitDialogue(n),
+                AST_Menu n => VisitMenu(n),
+                AST_MenuItem n => VisitMenuItem(n),
+                AST_Jump n => VisitJump(n),
+                AST_Tour n => VisitTour(n),
+                AST_Call n => VisitCall(n),
+                AST_Assign n => VisitAssign(n),
+                AST_If n => VisitIf(n),
+                AST_Expr_Or n => VisitExprOr(n),
+                AST_Expr_And n => VisitExprAnd(n),
+                AST_Expr_Equality n => VisitExprEquality(n),
+                AST_Expr_Comparison n => VisitExprComparison(n),
+                AST_Expr_Additive n => VisitExprAdditive(n),
+                AST_Expr_Multiplicative n => VisitExprMultiplicative(n),
+                AST_Expr_Power n => VisitExprPower(n),
+                AST_Expr_Unary n => VisitExprUnary(n),
+                AST_Literal n => VisitLiteral(n),
+                AST_FString n => VisitFString(n),
+                AST_EmbedCall n => VisitEmbedCall(n),
+                AST_EmbedExpr n => VisitEmbedExpr(n),
+                _ => throw new NotImplementedException($"No visit method for {node.GetType().Name}")
+            };
+        }
+        protected void VisitAllChildren(ASTNode node)
+        {
+            foreach (var child in node.Children)
+            {
+                Visit(child);
+            }
+        }
 
-        public virtual T VisitExprOr(AST_Expr_Or node) => default!;
-        public virtual T VisitExprAnd(AST_Expr_And node) => default!;
-        public virtual T VisitExprEquality(AST_Expr_Equality node) => default!;
-        public virtual T VisitExprComparison(AST_Expr_Comparison node) => default!;
-        public virtual T VisitExprAdditive(AST_Expr_Additive node) => default!;
-        public virtual T VisitExprMultiplicative(AST_Expr_Multiplicative node) => default!;
-        public virtual T VisitExprPower(AST_Expr_Power node) => default!;
-        public virtual T VisitExprUnary(AST_Expr_Unary node) => default!;
+        public virtual T VisitProgram(AST_Program node)
+        {
+            VisitAllChildren(node);
+            return default!;
+        }
+        public virtual T VisitImport(AST_Import node)
+        {
+            VisitAllChildren(node);
+            return default!;
+        }
+        public virtual T VisitLabelBlock(AST_LabelBlock node)
+        {
+            VisitAllChildren(node);
+            return default!;
+        }
+        public virtual T VisitDialogue(AST_Dialogue node)
+        {
+            VisitAllChildren(node);
+            return default!;
+        }
+        public virtual T VisitMenu(AST_Menu node)
+        {
+            VisitAllChildren(node);
+            return default!;
+        }
+        public virtual T VisitMenuItem(AST_MenuItem node)
+        {
+            VisitAllChildren(node);
+            return default!;
+        }
+        public virtual T VisitJump(AST_Jump node)
+        {
+            VisitAllChildren(node);
+            return default!;
+        }
+        public virtual T VisitTour(AST_Tour node)
+        {
+            VisitAllChildren(node);
+            return default!;
+        }
+        public virtual T VisitCall(AST_Call node)
+        {
+            VisitAllChildren(node);
+            return default!;
+        }
+        public virtual T VisitAssign(AST_Assign node)
+        {
+            VisitAllChildren(node);
+            return default!;
+        }
+        public virtual T VisitIf(AST_If node)
+        {
+            VisitAllChildren(node);
+            return default!;
+        }
 
-        public virtual T VisitLiteral(AST_Literal node) => default!;
-        public virtual T VisitFString(AST_FString node) => default!;
-        public virtual T VisitEmbedCall(AST_EmbedCall node) => default!;
-        public virtual T VisitEmbedExpr(AST_EmbedExpr node) => default!;
+        public virtual T VisitExprOr(AST_Expr_Or node)
+        {
+            VisitAllChildren(node);
+            return default!;
+        }
+        public virtual T VisitExprAnd(AST_Expr_And node)
+        {
+            VisitAllChildren(node);
+            return default!;
+        }
+        public virtual T VisitExprEquality(AST_Expr_Equality node)
+        {
+            VisitAllChildren(node);
+            return default!;
+        }
+        public virtual T VisitExprComparison(AST_Expr_Comparison node)
+        {
+            VisitAllChildren(node);
+            return default!;
+        }
+        public virtual T VisitExprAdditive(AST_Expr_Additive node)
+        {
+            VisitAllChildren(node);
+            return default!;
+        }
+        public virtual T VisitExprMultiplicative(AST_Expr_Multiplicative node)
+        {
+            VisitAllChildren(node);
+            return default!;
+        }
+        public virtual T VisitExprPower(AST_Expr_Power node)
+        {
+            VisitAllChildren(node);
+            return default!;
+        }
+        public virtual T VisitExprUnary(AST_Expr_Unary node)
+        {
+            VisitAllChildren(node);
+            return default!;
+        }
+
+        public virtual T VisitLiteral(AST_Literal node)
+        {
+            VisitAllChildren(node);
+            return default!;
+        }
+        public virtual T VisitFString(AST_FString node)
+        {
+            VisitAllChildren(node);
+            return default!;
+        }
+        public virtual T VisitEmbedCall(AST_EmbedCall node)
+        {
+            VisitAllChildren(node);
+            return default!;
+        }
+        public virtual T VisitEmbedExpr(AST_EmbedExpr node)
+        {
+            VisitAllChildren(node);
+            return default!;
+        }
     }
 }
